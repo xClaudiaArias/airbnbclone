1cf9d61d1441a542ed8592f26698be2577954c8c
claudia <xclaudiaarias@gmail.com>
2022-04-04 15:40:33 -0400

    getwaways navbar complete

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,8 @@ $(".nav-item").on('click', function(e){
     $(".nav-item").removeClass("active")
     $(e.target).addClass("active")
 })
+
+$(".getaways-links").on('click', function(e){
+    $(".getaways-links").removeClass("active-link")
+    $(e.target).addClass("active-link")
+})
